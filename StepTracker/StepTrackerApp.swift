892bc8d6d7534bd43deded7005b235ea75789392
Camille <verve.ton.6c@icloud.com>
2026-03-13 13:41:48 +0100

    Initial Commit

---
+++ StepTracker/StepTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepTrackerApp.swift
+//  StepTracker
+//
+//  Created by CamilleB on 13/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct StepTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
